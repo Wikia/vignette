19e516854a6ad52040755d5687ccaee9361b3b60
nelson <nelson@wikia-inc.com>
2014-07-24 17:20:27 +0200

    fixing indentation and adding exceptions

--- src/vignette/storage/local.clj
+++ src/vignette/storage/local.clj
@@ -6,18 +6,22 @@
             [clojure.java.shell :as sh])
   (:import  (java.io FileInputStream)))
 
+; TODO: when the logger has a closure port, we should create an exception that has context that we can log w/ exceptions
 (defrecord LocalObjectStorage [directory]
   ObjectStorageProtocol
 
- (get-object [this bucket path]
-   (let [real-file (io/file (resolve-local-path (:directory this) bucket path))]
-     (when (file-exists? real-file)
-       real-file)))
+  (get-object [this bucket path]
+    (let [real-file (io/file (resolve-local-path (:directory this) bucket path))]
+      (if (file-exists? real-file)
+        real-file
+        (throw (Exception. "get-object failed")))))
 
- (put-object [this resource bucket path]
-   (let [real-path (resolve-local-path (:directory this) bucket path)]
-     (create-local-path (get-parent real-path))
-     (transfer! resource real-path)))
+  (put-object [this resource bucket path]
+    (let [real-path (resolve-local-path (:directory this) bucket path)]
+      (create-local-path (get-parent real-path))
+      (if (transfer! resource real-path)
+        true
+        (throw (Exception. "put-object failed")))))
 
   (delete-object [this bucket path]
     (let [real-path (resolve-local-path (:directory this) bucket path)]
