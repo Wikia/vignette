406838d3af3d3c9a12c6e503450fda7813617f7e
Damon Snyder <drsnyder@gmail.com>
2014-07-24 18:36:55 +0200

    Adding unit test for app-routes.
    WIP. This is mostly an example to help us fill out these tests.
    
    /cc nmonterroso

--- test/vignette/http/routes_test.clj
+++ test/vignette/http/routes_test.clj
@@ -1,5 +1,8 @@
 (ns vignette.http.routes-test
-  (:require [vignette.http.routes :refer :all]
+  (:require [vignette.media-types :as mt]
+            [vignette.http.routes :refer :all]
+            [vignette.storage.protocols :refer :all]
+            [vignette.protocols :refer :all]
             [midje.sweet :refer :all]
             [clout.core :refer (route-compile route-matches)]
             [ring.mock.request :refer :all]))
@@ -24,3 +27,14 @@
                                                                                            :width "250"
                                                                                            :height "250"
                                                                                            }))
+
+(facts :app-routes
+  ((app-routes nil) (request :get "/does-not-exist")) => (contains {:status 404})
+
+  (let [route-params {:type :original, :original "ropes.jpg", :middle-dir "35", :top-dir "3", :wikia "lotr"} ]
+    ((app-routes ..system..) (request :get "/lotr/3/35/ropes.jpg")) => (contains {:status 200})
+
+    (provided
+     (store ..system..) => ..store..
+     (mt/get-media-map route-params) => route-params
+     (get-original ..store.. route-params) => "image-samples/ropes.jpg")))
