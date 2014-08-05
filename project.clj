cad07631334b825480af45fec608012b849cfb70
nmonterroso <nelson@wikia-inc.com>
2014-08-05 12:02:49 -0700

    remove private repo

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories ^:replace [["snapshots" "http://dev-nelson:81/repository/snapshots"]
-                           ["releases" "http://dev-nelson:81/repository/internal"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clout "1.2.0"]
                  [http-kit "2.1.16"]
