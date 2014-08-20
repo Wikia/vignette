4c342d5bdd4e3b8124fe5fa715ac3503e6d572a3
nmonterroso <nelson@wikia-inc.com>
2014-08-20 16:56:48 -0700

    updating sandbox to work with new routes

--- public/sandbox.js
+++ public/sandbox.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 			return false;
 		}
 
-		var image_url = "/bucket/a/ab/"+image;
+		var image_url = "/bucket/a/ab/"+image+"/revision/latest";
 		var request_type = $('#request-type').val();
 		if (request_type != 0) {
 			image_url += '/'+request_type;
