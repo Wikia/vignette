4dc575b27a3748d58099f9603c5bced2146bbf33
nmonterroso <nelson@wikia-inc.com>
2014-09-12 14:28:26 -0700

    reducing code duplication and fixing unit tests

--- src/vignette/storage/core.clj
+++ src/vignette/storage/core.clj
@@ -8,17 +8,19 @@
   (clojure.string/join "/" s))
 
 (defn get*
-  [store bucket prefix path]
+  [store object-map prefix get-path]
   (get-object store
-              bucket
-              (join-slash prefix path)))
+              (mt/wikia object-map)
+              (join-slash (q/query-opts->image-prefix object-map prefix)
+                          (get-path object-map))))
 
 (defn put*
-  [store resource bucket prefix path]
+  [store resource object-map prefix get-path]
   (put-object store
               resource
-              bucket
-              (join-slash prefix path)))
+              (mt/wikia object-map)
+              (join-slash (q/query-opts->image-prefix object-map prefix)
+                          (get-path object-map))))
 
 (defrecord ImageStorage [store original-prefix thumb-prefix]
   ImageStorageProtocol
@@ -26,28 +28,28 @@
   (save-thumbnail [this resource thumb-map]
     (put* (:store this)
           resource
-          (mt/wikia thumb-map)
-          (q/query-opts->image-prefix thumb-map (:thumb-prefix this))
-          (mt/thumbnail-path thumb-map)))
+          thumb-map
+          (:thumb-prefix this)
+          mt/thumbnail-path))
 
   (get-thumbnail [this thumb-map]
     (get* (:store this)
-          (mt/wikia thumb-map)
-          (q/query-opts->image-prefix thumb-map (:thumb-prefix this))
-          (mt/thumbnail-path thumb-map)))
+          thumb-map
+          (:thumb-prefix this)
+          mt/thumbnail-path))
 
   (save-original [this resource original-map]
     (put* (:store this)
           resource
-          (mt/wikia original-map)
-          (q/query-opts->image-prefix original-map (:original-prefix this))
-          (mt/original-path original-map)))
+          original-map
+          (:original-prefix this)
+          mt/original-path))
 
   (get-original [this original-map]
     (get* (:store this)
-          (mt/wikia original-map)
-          (q/query-opts->image-prefix original-map (:original-prefix this))
-          (mt/original-path original-map))))
+          original-map
+          (:original-prefix this)
+          mt/original-path)))
 
 (defn create-image-storage
   ([store original-prefix thumb-prefix]
