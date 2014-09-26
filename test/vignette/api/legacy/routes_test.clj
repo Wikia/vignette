c6a579d5a3fdf85fd57d58e3b87815a51383b790
nmonterroso <nelson@wikia-inc.com>
2014-09-26 14:07:16 -0700

    removing old tests

--- test/vignette/api/legacy/routes_test.clj
+++ test/vignette/api/legacy/routes_test.clj
@@ -68,30 +68,3 @@
          (:original map) => "Flag_of_Europe.svg"
          (:revision map) => "latest"
          (:lang (:options map)) => "fr"))
-
-;(facts :image-thumbnail-width
-;  (let [rt (routes/add-request-type {} :image-thumbnail)]
-;    (routes/request-map-add-width
-;       (merge rt {:thumbnail "130px-102,382,0,247-Brain_Attack.PNG"} ))  => (contains {:width "130px"})
-;    (routes/request-map-add-width
-;       (merge rt {:thumbnail "10x10-102,382,0,247-Brain_Attack.PNG"} ))  => (contains {:width "10x10"})
-;    (routes/request-map-add-width
-;       (merge rt {:thumbnail "10x10x10-102,382,0,247-Brain_Attack.PNG"} ))  => (contains {:width "10x10x10"})
-;    (routes/request-map-add-width
-;       {:thumbnail "10x10x10-102,382,0,247-Brain_Attack.PNG"})  => (contains {:width ""})))
-;
-;(facts :coordinates
-;  (routes/thumbnail-path->coordinates "") => falsey
-;  (routes/thumbnail-path->coordinates "0,0,0") => falsey
-;  (routes/thumbnail-path->coordinates "0,0,0,0") => (contains {:x1 0 :x2 0 :y1 0 :y2 0})
-;  (routes/thumbnail-path->coordinates "1,2,3,4") => (contains {:x1 1 :x2 2 :y1 3 :y2 4}))
-;
-;(facts :dimensions
-;  (routes/thumbnail-path->dimensions "") => falsey
-;  (routes/thumbnail-path->dimensions "foobar.png") => falsey
-;  (routes/thumbnail-path->dimensions "123px-foobar.png") => truthy
-;  (routes/thumbnail-path->dimensions "123px-foobar.png") => [:width '(123)]
-;  (routes/thumbnail-path->dimensions "123x123-foobar.png") => [:width-height '(123 123)])
-
-; this is specific to the no-extension request type
-; (routes/thumbnail-path->dimensions "123x124x10-foobar") => [:width-height-scale '(123 124 10)]
