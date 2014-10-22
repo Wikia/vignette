66e7417dd5a12155f90cbb59923bf81fdf13fcb0
Damon Snyder <drsnyder@gmail.com>
2014-10-22 15:32:26 -0700

    Change mode to window-crop-fixed.

--- src/vignette/http/routes.clj
+++ src/vignette/http/routes.clj
@@ -59,7 +59,7 @@
                   :window-height size-regex}))
 
 (def window-crop-fixed-route
-  (route-compile "/:wikia:image-type/:top-dir/:middle-dir/:original/revision/:revision/window-crop/width/:width/height/:height/x-offset/:x-offset/y-offset/:y-offset/window-width/:window-width/window-height/:window-height"
+  (route-compile "/:wikia:image-type/:top-dir/:middle-dir/:original/revision/:revision/window-crop-fixed/width/:width/height/:height/x-offset/:x-offset/y-offset/:y-offset/window-width/:window-width/window-height/:window-height"
                  {:wikia wikia-regex
                   :image-type image-type-regex
                   :top-dir top-dir-regex
