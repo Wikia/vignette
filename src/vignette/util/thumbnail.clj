3de7f2faf163e5e4f9e1a0432ef13c3fc0730622
Pawel Chojnacki <pawel@chojnacki.ws>
2015-10-21 22:20:14 +0200

    Revert "Merge pull request #100 from Wikia/revert-99-suffix_filename"
    This reverts commit f150895a98d16cdd2fa5830d57d877d7d73e978c, reversing
    changes made to 0c18d8a73038077758bbdb81b206e12184e36251.

--- src/vignette/util/thumbnail.clj
+++ src/vignette/util/thumbnail.clj
@@ -112,7 +112,7 @@
 (defn original->local
   "Take the original and make it local."
   [original]
-  (let [temp-file (io/file (temp-filename))]
+  (let [temp-file (io/file (temp-filename nil (filename original)))]
     (when (transfer! original temp-file)
       temp-file)))
 
