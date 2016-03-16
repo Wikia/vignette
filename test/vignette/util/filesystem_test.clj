11b73222384a66606724292f8e79b3d216cf381c
Pawel Chojnacki <pawel@chojnacki.ws>
2016-03-16 17:03:13 +0100

    add tests

--- test/vignette/util/filesystem_test.clj
+++ test/vignette/util/filesystem_test.clj
@@ -5,4 +5,5 @@
 (facts :file-extension
        (file-extension "some-file.png") => "png"
        (file-extension "Mambo No. 5 (A Little Bit of...)") => nil
-       (file-extension "some-file.png.jpg") => "jpg")
+       (file-extension "some-file.png.jpg") => "jpg"
+       (file-extension nil) => nil)
