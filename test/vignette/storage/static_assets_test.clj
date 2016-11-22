6b897d709eef13a594bff15e4b2100b5f716f825
Adam Skubicha <askubicha@wikia-inc.com>
2016-11-22 15:24:04 +0100

    SER-532 Brings back changes with required statuses from image-review for vignette to return an image.

--- test/vignette/storage/static_assets_test.clj
+++ test/vignette/storage/static_assets_test.clj
@@ -38,12 +38,13 @@
          (http/get ..static-asset-url.. {:as :stream}) => (future {:status 404})
          (http/get ..image-review-url..) => (future {:status 200}))
        (get-original
-         (sa/create-static-image-storage --static-asset-get-- --image-review-get--) {:uuid ..uuid.. :options {:status ..statuses..}}) => nil
+         (sa/create-static-image-storage --static-asset-get-- --image-review-get--) {:uuid ..uuid.. :options {:status ..statuses..}}) => ..object..
        (provided
          (--static-asset-get-- ..uuid..) => ..static-asset-url..
          (--image-review-get-- ..uuid.. ..statuses..) => ..image-review-url..
          (http/get ..static-asset-url.. {:as :stream}) => (future {:status 200})
-         (http/get ..image-review-url..) => (future {:status 401}))
+         (http/get ..image-review-url..) => (future {:status 401})
+         (sa/->AsyncResponseStoredObject {:status 200}) => ..object..)
        (get-original
          (sa/create-static-image-storage --static-asset-get-- --image-review-get--) {:uuid ..uuid.. :options {:status ..statuses..}}) => nil
        (provided
