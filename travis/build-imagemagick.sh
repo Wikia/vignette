f59e4dca9a64b92d81ad1fabeb6e3025c51e17fb
Łukasz Harasimowicz <harnash@users.noreply.github.com>
2017-02-03 11:12:50 +0100

    Platform-2817: Travis integration tests fixes (#123)
    * PLATFORM-2817: adding ImageMagic+WebP binaries to Travis-CI
    * Moving ImageMagick build commands into separate script
    * Allowing size image to differ more in integration tests
    Depending on ImageMagic + WebP lib version we might get different
    image sizes - we should check that with greater tolerance.

---
+++ travis/build-imagemagick.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export PATH=$HOME/opt/bin:$PATH
+export LDFLAGS=-L$HOME/opt/lib
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:$HOME/opt/lib
+export CPPFLAGS=-I$HOME/opt/include
+export CFLAGS=-I$HOME/opt/include
+export CORES=$(nproc)
+
+echo "Using $CORES cores for compiling..."
+
+cd /tmp
+curl -O https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$LIBWEBP_VERSION.tar.gz
+tar xvzf libwebp-$LIBWEBP_VERSION.tar.gz
+cd libwebp-$LIBWEBP_VERSION
+./configure --prefix=$HOME/opt
+make -j$CORES
+make install -j$CORES
+cd /tmp
+curl -O https://www.imagemagick.org/download/ImageMagick-$IMAGEMAGICK_VERSION.tar.gz
+tar xvzf ImageMagick-$IMAGEMAGICK_VERSION.tar.gz
+cd ImageMagick-$IMAGEMAGICK_VERSION
+./configure --prefix=$HOME/opt --with-webp
+make -j$CORES
+make install -j$CORES
