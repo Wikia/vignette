304039221553155030257438418254acca22d760
Łukasz Harasimowicz <dev@harnash.eu>
2017-03-17 11:19:24 +0100

    PLATFORM-2890: initial webp implementation for legacy urls

--- src/vignette/http/legacy/route_helpers.clj
+++ src/vignette/http/legacy/route_helpers.clj
@@ -2,10 +2,12 @@
   (:require [useful.experimental :refer [cond-let]]
             [vignette.protocols :refer :all]
             [vignette.storage.protocols :refer :all]
-            [vignette.media-types :refer [archive-dir]])
+            [vignette.media-types :refer [archive-dir]]
+            [vignette.media-types :as mt])
   (:import [java.net URLDecoder]))
 
 (def default-width 200)
+(def webp-accept-header-name "accept")
 
 (declare route->revision
          route->dimensions
@@ -21,6 +23,26 @@
 (defn archive? [map]
   (= (:zone map) (str "/" archive-dir)))
 
+(defn browser-supports-webp? [request]
+  (if-let [vary-string (get-in request [:headers webp-accept-header-name])]
+    (.contains vary-string mt/webp-mime-type)))
+
+(defn add-webp-format-option-if-supported
+  [request options]
+  (if (browser-supports-webp? request)
+    (merge options {:format mt/webp-format})
+    options))
+
+(defn autodetect-request-format
+  [request options]
+  (add-webp-format-option-if-supported request options))
+
+(defn route->webp-request-format
+  "In case format was not specified in query options, try to set it to webp based on the accept header."
+  [request-map request]
+  (assoc request-map :requested-format (:format (:options request-map))
+                     :options (autodetect-request-format request (:options request-map))))
+
 (defn zone [map]
   (if (and (not (empty? (:zone map)))
            (not (archive? map)))
@@ -30,7 +52,7 @@
     nil))
 
 (defn route->thumb-map
-  [route-params]
+  [route-params request]
   ; order is important! mostly due to the different options changing :thumbnail-mode
   (let [map (-> route-params
                 (assoc :request-type :thumbnail)
@@ -38,39 +60,44 @@
                 (route->dimensions)
                 (route->offset)
                 (route->revision)
-                (route->options))]
+                (route->options)
+                (route->webp-request-format request))]
     map))
 
 (defn route->original-map
-  [route-params]
+  [route-params request]
   (let [map (-> route-params
                 (assoc :request-type :original)
                 (route->revision)
-                (route->options))]
+                (route->options)
+                (route->webp-request-format request))]
     map))
 
 (defn route->timeline-map
-  [route-params]
+  [route-params request]
   (-> route-params
       (assoc :request-type :original)
       (assoc :top-dir "timeline")
-      (route->options)))
+      (route->options)
+      (route->webp-request-format request)))
 
 (defn route->interactive-maps-map
-  [route-params]
+  [route-params request]
   (-> route-params
       (assoc :request-type :original)
       (assoc :image-type "arbitrary")
-      (route->options)))
+      (route->options)
+      (route->webp-request-format request)))
 
 (defn route->interactive-maps-thumbnail-map
-  [route-params]
+  [route-params request]
   (-> route-params
       (assoc :request-type :thumbnail)
       (assoc :image-type "arbitrary")
       (route->dimensions)
       (route->offset)
-      (route->options)))
+      (route->options)
+      (route->webp-request-format request)))
 
 
 (defn route->revision
