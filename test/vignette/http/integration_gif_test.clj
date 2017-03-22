136e7cc58bce66dd6b9e51d82bf33cab4e095e09
mech <wozniak.jacek@gmail.com>
2017-03-22 09:40:13 +0100

    Integration tests for gif

--- test/vignette/http/integration_gif_test.clj
+++ test/vignette/http/integration_gif_test.clj
@@ -22,34 +22,35 @@
 (with-state-changes [(before :facts (start system-local default-port))
                      (after :facts (stop system-local))]
 
-  (let [response (client/get (format "http://localhost:%d/bucket/a/ab/chuck-large-animated.gif/revision/latest" default-port) {:as :byte-array, :accept "image/webp"})]
-    (:status response) => 200
-    (get (:headers response) "Surrogate-Key") => "936b19e79decb9240a0b65ff437ae5ea033c0f69"
-    (get (:headers response) "Content-Disposition") => "inline; filename=\"chuck-large-animated.webp\"; filename*=UTF-8''chuck-large-animated.webp"
-    (Integer/parseInt (get (:headers response) "Content-Length")) => (roughly 1405790 50)
-    (get (:headers response) "Connection") => "close"
-    (get (:headers response) "Cache-Control") => "public, max-age=31536000"
-    (get (:headers response) "Content-Type") => "image/webp"
-    (vec (:body response)) => (has-prefix riff-header)
-    (vec (:body response)) => (contains webp-header))
+  (facts :requests :thumbnail-integration :original
+                   (let [response (client/get (format "http://localhost:%d/bucket/a/ab/chuck-large-animated.gif/revision/latest" default-port) {:as :byte-array, :accept "image/webp"})]
+                     (:status response) => 200
+                     (get (:headers response) "Surrogate-Key") => "936b19e79decb9240a0b65ff437ae5ea033c0f69"
+                     (get (:headers response) "Content-Disposition") => "inline; filename=\"chuck-large-animated.webp\"; filename*=UTF-8''chuck-large-animated.webp"
+                     (Integer/parseInt (get (:headers response) "Content-Length")) => (roughly 1405790 50)
+                     (get (:headers response) "Connection") => "close"
+                     (get (:headers response) "Cache-Control") => "public, max-age=31536000"
+                     (get (:headers response) "Content-Type") => "image/webp"
+                     (vec (:body response)) => (has-prefix riff-header)
+                     (vec (:body response)) => (contains webp-header))
 
-  (let [response (client/get (format "http://localhost:%d/bucket/a/ab/chuck-large-animated.gif/revision/latest/scale-to-width/200" default-port) {:as :byte-array, :accept "image/webp"})]
-    (:status response) => 200
-    (get (:headers response) "Surrogate-Key") => "936b19e79decb9240a0b65ff437ae5ea033c0f69"
-    (get (:headers response) "Content-Disposition") => "inline; filename=\"chuck-large-animated.webp\"; filename*=UTF-8''chuck-large-animated.webp"
-    (Integer/parseInt (get (:headers response) "Content-Length")) => (roughly 183990 50)
-    (get (:headers response) "Connection") => "close"
-    (get (:headers response) "Cache-Control") => "public, max-age=31536000"
-    (get (:headers response) "Content-Type") => "image/webp"
-    (vec (:body response)) => (has-prefix riff-header)
-    (vec (:body response)) => (contains webp-header))
+         (let [response (client/get (format "http://localhost:%d/bucket/a/ab/chuck-large-animated.gif/revision/latest/scale-to-width/200" default-port) {:as :byte-array, :accept "image/webp"})]
+           (:status response) => 200
+           (get (:headers response) "Surrogate-Key") => "936b19e79decb9240a0b65ff437ae5ea033c0f69"
+           (get (:headers response) "Content-Disposition") => "inline; filename=\"chuck-large-animated.webp\"; filename*=UTF-8''chuck-large-animated.webp"
+           (Integer/parseInt (get (:headers response) "Content-Length")) => (roughly 183990 50)
+           (get (:headers response) "Connection") => "close"
+           (get (:headers response) "Cache-Control") => "public, max-age=31536000"
+           (get (:headers response) "Content-Type") => "image/webp"
+           (vec (:body response)) => (has-prefix riff-header)
+           (vec (:body response)) => (contains webp-header))
 
-  (let [response (client/get (format "http://localhost:%d/bucket/a/ab/chuck-large-animated.gif/revision/latest/scale-to-width/200" default-port) {:as :byte-array})]
-    (:status response) => 200
-    (get (:headers response) "Surrogate-Key") => "936b19e79decb9240a0b65ff437ae5ea033c0f69"
-    (get (:headers response) "Content-Disposition") => "inline; filename=\"chuck-large-animated.gif\"; filename*=UTF-8''chuck-large-animated.gif"
-    (Integer/parseInt (get (:headers response) "Content-Length")) => (roughly 519260 50)
-    (get (:headers response) "Connection") => "close"
-    (get (:headers response) "Cache-Control") => "public, max-age=31536000"
-    (get (:headers response) "Content-Type") => "image/gif"
-    (vec (:body response)) => (contains gif-header)))
+         (let [response (client/get (format "http://localhost:%d/bucket/a/ab/chuck-large-animated.gif/revision/latest/scale-to-width/200" default-port) {:as :byte-array})]
+           (:status response) => 200
+           (get (:headers response) "Surrogate-Key") => "936b19e79decb9240a0b65ff437ae5ea033c0f69"
+           (get (:headers response) "Content-Disposition") => "inline; filename=\"chuck-large-animated.gif\"; filename*=UTF-8''chuck-large-animated.gif"
+           (Integer/parseInt (get (:headers response) "Content-Length")) => (roughly 519260 50)
+           (get (:headers response) "Connection") => "close"
+           (get (:headers response) "Cache-Control") => "public, max-age=31536000"
+           (get (:headers response) "Content-Type") => "image/gif"
+           (vec (:body response)) => (contains gif-header))))
