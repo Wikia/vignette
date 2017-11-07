228538aeac3b1e4e52139ff19f3799f540aa1427
Michał Turek <mturek@wikia-inc.com>
2017-11-07 17:50:33 +0100

    SER-1338 | Fix ()

--- test/vignette/storage/static_assets_test.clj
+++ test/vignette/storage/static_assets_test.clj
@@ -6,7 +6,7 @@
             ))
 
 (facts :static-assets :get-original
-       (let [store (create-image-storage ..disk-store..)]
+     (let [store (create-image-storage ..disk-store..)]
 
        (get-original
          (sa/create-static-image-storage store --static-asset-get--) {:uuid ..uuid..}) => ..object..
@@ -18,7 +18,7 @@
          (sa/create-static-image-storage store --static-asset-get--) {:uuid ..uuid.. :options {:status ..statuses..}}) => nil
        (provided
          (--static-asset-get-- ..uuid..) => ..static-asset-url..
-         (http/get ..static-asset-url.. {:as :stream}) => (future {:status 404})))
+         (http/get ..static-asset-url.. {:as :stream}) => (future {:status 404}))))
 
 (facts :static-assets :filename
        (filename
@@ -37,7 +37,8 @@
          (sa/->AsyncResponseStoredObject ..response..)) => nil)
 
 (facts :static-assets :get-blocked-placeholder
-       (let [store (create-image-storage ..disk-store..)]
+     (let [store (create-image-storage ..disk-store..)]
+
        (get-original
          (sa/create-static-image-storage store --static-asset-get--) {:uuid ..uuid.. :blocked-placeholder ..placeholder-id..}) => ..placeholder..
        (provided
@@ -45,4 +46,4 @@
          (http/get ..static-asset-url.. {:as :stream}) => (future {:status 451})
          (--static-asset-get-- ..placeholder-id..) => ..placeholder-url..
          (http/get ..placeholder-url.. {:as :stream}) => (future {:status 200})
-         (sa/->AsyncResponseStoredObject {:status 200}) => ..placeholder..))
+         (sa/->AsyncResponseStoredObject {:status 200}) => ..placeholder..)))
