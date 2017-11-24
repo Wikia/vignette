89225af08cda4d047a278d067b9d43b6ee0c61ac
Adam Robak <robakadam@gmail.com>
2017-11-24 14:02:35 +0100

    SER-1418 | use string formatter instead of concat, fix tests

--- src/vignette/util/services.clj
+++ src/vignette/util/services.clj
@@ -1,14 +1,13 @@
 (ns vignette.util.services
   (:require [clojure.string :refer [join]]
-            [environ.core :refer [env]]))
+    [environ.core :refer [env]]))
 
 
 (defn get-domain []
   (let [environment (env :wikia-environment)
         datacenter (env :wikia-datacenter)]
-    (join "." [environment
-               (if (= environment "dev") (join "-" [datacenter environment]) datacenter)
-               "k8s.wikia.net"])))
+    (format "http://%s.%s.k8s.wikia.net" environment
+      (if (= environment "dev") (join "-" [datacenter environment]) datacenter))))
 
 (defn materialize-static-asset-url [oid]
   (str (get-domain) "/static-assets/image/" oid))
--- test/vignette/util/services_test.clj
+++ test/vignette/util/services_test.clj
@@ -4,13 +4,13 @@
             [vignette.util.services :as svc]))
 
 (facts :static-assets :url
-       (svc/materialize-static-asset-url "uuid_sample") => "dev.test-dev.k8s.wikia.net/static-assets/image/uuid_sample"
+       (svc/materialize-static-asset-url "uuid_sample") => "http://dev.test-dev.k8s.wikia.net/static-assets/image/uuid_sample"
        (provided
          (env :wikia-environment) => "dev"
          (env :wikia-datacenter) => "test")
 
 
-       (svc/materialize-static-asset-url "uuid_sample") => "prod.test.k8s.wikia.net/static-assets/image/uuid_sample"
+       (svc/materialize-static-asset-url "uuid_sample") => "http://prod.test.k8s.wikia.net/static-assets/image/uuid_sample"
        (provided
          (env :wikia-environment) => "prod"
          (env :wikia-datacenter) => "test"))
