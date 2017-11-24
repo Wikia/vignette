87afd55191b0970b2111c01684acf9590a8633a7
Adam Robak <robakadam@gmail.com>
2017-11-24 16:27:11 +0100

    SER-1418 | move dc calculation to let, squash into a single method

--- src/vignette/util/services.clj
+++ src/vignette/util/services.clj
@@ -1,13 +1,10 @@
 (ns vignette.util.services
   (:require [clojure.string :refer [join]]
-    [environ.core :refer [env]]))
+            [environ.core :refer [env]]))
 
-
-(defn get-domain []
-  (let [environment (env :wikia-environment)
-        datacenter (env :wikia-datacenter)]
-    (format "http://%s.%s.k8s.wikia.net" environment
-      (if (= environment "dev") (join "-" [datacenter environment]) datacenter))))
 
 (defn materialize-static-asset-url [oid]
-  (str (get-domain) "/static-assets/image/" oid))
+  (let [environment (env :wikia-environment)
+        physical-dc (env :wikia-datacenter)
+        logical-dc (if (= environment "dev") (join "-" [physical-dc environment]) physical-dc)]
+    (format "http://%s.%s.k8s.wikia.net/static-assets/image/%s" environment logical-dc oid)))
