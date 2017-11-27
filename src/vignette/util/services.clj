452add117716a398bfd2741a79ed141ccfe0f5fe
Adam Robak <robakadam@gmail.com>
2017-11-27 16:14:28 +0100

    SER-1418 | create domain on startup

--- src/vignette/util/services.clj
+++ src/vignette/util/services.clj
@@ -1,12 +1,15 @@
 (ns vignette.util.services
   (:require [clojure.string :refer [join]]
-    [environ.core :refer [env]]))
+            [environ.core :refer [env]]))
 
-
-(defn materialize-static-asset-url [oid]
+(defn materialize-static-asset-domain []
   (let [environment (env :wikia-environment)
         physical-dc (env :wikia-datacenter)
-        logical-dc (if (= environment "dev") (clojure.string/join "-" [physical-dc environment]) physical-dc)]
+        logical-dc (if (= environment "dev") (join "-" [physical-dc environment]) physical-dc)]
     (if (empty? environment) (throw (Exception. "WIKIA_ENVIRONMENT not set"))
       (if (empty? physical-dc) (throw (Exception. "WIKIA_DATACENTER not set"))
-        (format "http://%s.%s.k8s.wikia.net/static-assets/image/%s" environment logical-dc oid)))))
+        (format "http://%s.%s.k8s.wikia.net/static-assets/image" environment logical-dc)))))
+
+(defn materialize-static-asset-url []
+  (let [domain (materialize-static-asset-domain)]
+    (fn [oid] (format "%s/%s" domain oid))))
