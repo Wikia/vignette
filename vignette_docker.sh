7c8ca9b8e773c88f6676f478e7260929180f3493
Adam Robak <robakadam@gmail.com>
2018-08-03 09:38:06 +0200

    prepare vignette for running in k8s

---
+++ vignette_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run -e "STORAGE_ACCESS_KEY=6J1ZCE0KZZYG2U9CHJGA" \
+-e "STORAGE_SECRET_KEY=gpCJjIptrr5S7XOGYG2bH4yXnru+YN/O7hBNyPyf" \
+-e "STORAGE_ENDPOINT=http://dfs" \
+-e "STORAGE_PROXY=dev-dfs-p6" \
+-e "STORAGE_PROXY_PORT=80" \
+-e "LOGGER_TYPE=stdout" \
+-e "LOGGER_FILE_OUTPUT=/tmp/vignette.log" \
+-e "LOGGER_APPLICATION=vignette" \
+-e "IMAGEMAGICK_BASE=/usr" \
+-e "VIGNETTE_THUMBNAIL_BIN=/thumbnail" \
+-e "GETOPT=/bin/getopt" \
+-e "TIMEOUT=timeout" \
+-e "TIMEOUT_TIME=-t 30" \
+-e "GIF2WEBP=/gif2webp" \
+-e "WIKIA_ENVIRONMENT=dev" \
+-e "WIKIA_DATACENTER=poz" -it -p 8080:8080 artifactory.wikia-inc.com/vignette:0.3
